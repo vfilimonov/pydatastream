778e220e3f9c960a86bbd4ee777330e5fa297c86
Vladimir Filimonov <vfilimonov@ethz.ch>
2015-03-19 18:32:55 +0100

    Parse record for static request

--- pydatastream/pydatastream.py
+++ pydatastream/pydatastream.py
@@ -290,6 +290,61 @@ class Datastream:
         metadata = metadata[['Symbol','DisplayName','Currency','Frequency','Status']]
         return data, metadata
 
+    def parse_record_static(self, raw):
+        """Parse raw data (that is retrieved by static request) and return pandas.DataFrame.
+           Returns tuple (data, metadata)
+
+           data - pandas.DataFrame with retrieved data.
+           metadata - pandas.DataFrame with info about symbol, currency, frequency,
+                      displayname and status of given request
+        """
+        ### Parsing status
+        status = self.status(raw)
+
+        ### Testing if no errors
+        if status['StatusType'] != 'Connected':
+            if self.raise_on_error:
+                raise DatastreamException('%s (error %i): %s --> "%s"' %
+                                          (status['StatusType'], status['StatusCode'],
+                                           status['StatusMessage'], status['Request']))
+            else:
+                self._test_status_and_warn()
+                return pd.DataFrame(), {}
+
+        ### Convert record to dict
+        record = self.extract_data(raw)
+
+        try:
+            error = record['INSTERROR']
+            if self.raise_on_error:
+                raise DatastreamException('Error: %s --> "%s"' %
+                                          (error, status['Request']))
+            else:
+                self.last_status['StatusMessage'] = error
+                self.last_status['StatusType'] = 'INSTERROR'
+                self._test_status_and_warn()
+                return pd.DataFrame(), {'Status': error, 'Date': None}
+        except KeyError:
+            metadata = {'Status': 'OK', 'Date': None}
+
+        ### All fields that are available
+        fields = [x for x in record if '_' not in x]
+        metadata['Date'] = record['DATE']
+        fields.remove('DATE')
+
+        ### Number of elements
+        num = len([x[0] for x in record if 'SYMBOL' in x])
+
+        ### field naming 'CCY', 'CCY_2', 'CCY_3', ...
+        fld_name = lambda field, indx: field if indx == 0 else field+'_%i'%(indx+1)
+
+        ### Construct pd.DataFrame
+        res = pd.DataFrame({fld:[record[fld_name(fld, ind)]
+                                 if fld_name(fld, ind) in record else ''
+                                 for ind in range(num)]
+                            for fld in fields})
+        return res, metadata
+
     #====================================================================================
     @staticmethod
     def construct_request(ticker, fields=None, date=None,
