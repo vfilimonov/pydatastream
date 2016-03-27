739b4d6a16762ea35fccb70c13fd8e9b610274ab
Vladimir Filimonov <vladimir.a.filimonov@gmail.com>
2016-03-27 21:20:15 +0200

    Version bump: 0.4.1

--- setup.py
+++ setup.py
@@ -6,18 +6,16 @@ if sys.version_info.major < 3:
 else:
     install_requires_list = ['suds-py3', 'pandas']
 
-setup(name = 'PyDatastream',
-      version = '0.4.0',
-      description = 'Python interface to the Thomson Reuters Dataworks Enterprise (Datastream) API',
-      url = 'http://github.com/vfilimonov/pydatastream',
-      download_url = 'https://github.com/vfilimonov/pydatastream/archive/v0.4.0.zip',
-      author = 'Vladimir Filimonov',
-      author_email = 'vladimir.a.filimonov@gmail.com',
-      license = 'MIT License',
-      packages = ['pydatastream'],
-      install_requires = install_requires_list,
-      classifiers=[
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-    ]
-     )
+setup(name='PyDatastream',
+      version='0.4.1',
+      description='Python interface to the Thomson Reuters Dataworks Enterprise (Datastream) API',
+      url='http://github.com/vfilimonov/pydatastream',
+      download_url='https://github.com/vfilimonov/pydatastream/archive/v0.4.1.zip',
+      author='Vladimir Filimonov',
+      author_email='vladimir.a.filimonov@gmail.com',
+      license='MIT License',
+      packages=['pydatastream'],
+      install_requires=install_requires_list,
+      classifiers=['Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 3', ]
+      )
